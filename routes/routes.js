8d6bccaf5729d9329f013ff334c8435b43ad0161
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2024-05-23 19:04:56 +0300

    added the template

---
+++ routes/routes.js
@@ -0,0 +1,17 @@
+// Import your routes here...
+const authRoutes = require("./authRoutes");
+const userRoutes = require("./userRoutes");
+const indexRoutes = require("./indexRoutes");
+
+function setupRoutes(app) {
+  // Initialize your routes here...
+  app.use("/", indexRoutes);
+  app.use("/auth", authRoutes);
+  app.use("/user", userRoutes);
+  // Catch-all route for handling 404 errors
+  app.use((req, res, next) => {
+    res.render("404", { currentPage: "404" });
+  });
+}
+
+module.exports = { setupRoutes };
